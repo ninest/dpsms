60e4c14debe78653b37e8c979446d201d5c2f6b2
Arjun Bhat <junar9989@gmail.com>
2023-08-20 23:49:32 +0530

    Changes

--- src/app/(main)/search/search-results.tsx
+++ src/app/(main)/search/search-results.tsx
@@ -2,19 +2,25 @@
 
 import { useSearchParams } from "next/navigation";
 
-export function SearchResult() {
+interface Props {
+    hostListings: [];
+}
+
+export function SearchResult({ hostListings }: Props) {
     const searchParams = useSearchParams()
     const latitude = searchParams.get('latitude')
     const longitude = searchParams.get('longitude')
     const location = searchParams.get('location')
     const qualifiers = searchParams.get('qualifiers')
 
-    // Ensure that they meet all the 
+    // Ensure that they meet all the qualifier requirements
     // Access the database and get all the address latitudes/longitudes
     // Sort by distance
     // Display listings in order
 
     const randomString = `Latitude: ${latitude} Longitude: ${longitude}`
+
+    // const sortedListings = hostListings.sort(function ());
 
     return (
         <div>
