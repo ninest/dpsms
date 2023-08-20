ebbe912b828a7ea2ec8151cc260a6fbf16327621
ninest <parth.kabra@gmail.com>
2023-08-20 19:13:05 -0400

    Get searchParams in server component

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -2,16 +2,32 @@ import { SearchForm } from "@/app/(main)/search/search-form";
 import { SearchResult } from "@/app/(main)/search/search-results";
 import { usersService } from "@/services/users-service";
 import { auth } from "@clerk/nextjs";
-import { Suspense } from 'react'
+import { Suspense } from "react";
 
-export default async function SearchPage() {
+interface Props {
+  searchParams: {
+    latitude?: string;
+    longitude?: string;
+    location?: string;
+    qualifiers?: string[];
+  };
+}
+
+export default async function SearchPage({ searchParams }: Props) {
   const { userId: clerkId } = auth();
   const address = clerkId ? (await usersService.getUserByClerkId(clerkId)).address : "";
 
+  let hostListings = [];
+  if (Object.keys(searchParams).length > 0) {
+    const { latitude, longitude, location, qualifiers } = searchParams;
+    // Call search service and update hostListings
+    // hostListings = []
+  }
+
   return (
-    <Suspense fallback="" >
+    <Suspense fallback="">
       <SearchForm defaultAddress={address} />
-      <SearchResult />
+      <SearchResult hostListings={[]} />
     </Suspense>
   );
 }
--- src/app/(main)/search/search-results.tsx
+++ src/app/(main)/search/search-results.tsx
@@ -3,30 +3,16 @@
 import { useSearchParams } from "next/navigation";
 
 interface Props {
-    hostListings: [];
+  hostListings: [];
 }
 
 export function SearchResult({ hostListings }: Props) {
-    const searchParams = useSearchParams()
-    const latitude = searchParams.get('latitude')
-    const longitude = searchParams.get('longitude')
-    const location = searchParams.get('location')
-    const qualifiers = searchParams.get('qualifiers')
+  // Ensure that they meet all the qualifier requirements
+  // Access the database and get all the address latitudes/longitudes
+  // Sort by distance
+  // Display listings in order
 
-    // Ensure that they meet all the qualifier requirements
-    // Access the database and get all the address latitudes/longitudes
-    // Sort by distance
-    // Display listings in order
+  // const sortedListings = hostListings.sort(function ());
 
-    const randomString = `Latitude: ${latitude} Longitude: ${longitude}`
-
-    // const sortedListings = hostListings.sort(function ());
-
-    return (
-        <div>
-            <h1> {randomString} </h1>
-            <h2> {location} </h2>
-            <h3> {qualifiers} </h3>
-        </div>
-    );
+  return <div></div>;
 }
