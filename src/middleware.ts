93e851f325616c378bb71ec15c757d64a24ada60
ninest <parth.kabra@gmail.com>
2023-08-21 17:45:39 -0400

    Disallow edit profile page for anon

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/(api|trpc)(.*)", "/", "/hosts/(.*)", "/search" ,'/profile/(.*)'],
+  publicRoutes: ["/(api|trpc)(.*)", "/", "/hosts/(.*)", "/search" ,'/profile/((?!edit).*)'],
 });
 
 export const config = {
