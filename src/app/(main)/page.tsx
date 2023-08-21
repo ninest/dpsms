b573b77685176f448dd059d03dc226518c8f7188
ninest <parth.kabra@gmail.com>
2023-08-21 10:14:40 -0400

    Add host listing page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -23,58 +23,67 @@ export default async function Home() {
   const isProfileComplete = clerkId ? await usersService.profileComplete(clerkId) : false;
   const isActiveHost = clerkId ? await usersService.isActiveHost(clerkId) : false;
 
+  const hostListingButton = (
+    <Button asChild className="w-full lg:w-auto">
+      <Link href={"/new-host"}>New Host Listing</Link>
+    </Button>
+  );
+
   return (
     <main>
       {/* <div className="flex items-center justify-center mb-4">
         <Title level={1}>{welcomeMessage}</Title>
       </div> */}
-      <div className=" border rounded-md p-6">
-        <h1 className="font-semibold text-2xl">DPSMS</h1>
-        <Spacer className="h-2" />
-        <div className="space-y-1">
-          <p>A decentralized storage network designed to store your stuff.</p>
-          {isAuthenticated ? (
-            <>
-              {!isProfileComplete && (
-                <p>
-                  <Link href={"/profile"} className="text-gray-500 text-sm">
-                    Your profile is incomplete. Go to your <span className="underline">profile page</span> to complete
-                    it!
-                  </Link>
-                </p>
-              )}
-              {isActiveHost ? (
+      <div className="border rounded-md p-6">
+        <div className="flex justify-between align-top">
+          <div>
+            <h1 className="font-semibold text-2xl">DPSMS</h1>
+            <Spacer className="h-2" />
+            <div className="space-y-1">
+              <p>A decentralized storage network designed to store your stuff.</p>
+              {isAuthenticated ? (
                 <>
-                  <Spacer className="h-2" />
-                  <Button asChild className="w-full">
-                    <Link href={"new-host"}>New Host Listing</Link>
-                  </Button>
+                  {!isProfileComplete && (
+                    <p>
+                      <Link href={"/profile"} className="text-gray-500 text-sm">
+                        Your profile is incomplete. Go to your <span className="underline">profile page</span> to
+                        complete it!
+                      </Link>
+                    </p>
+                  )}
+                  {isActiveHost ? (
+                    <div className="lg:hidden">
+                      <Spacer className="h-2" />
+                      {hostListingButton}
+                    </div>
+                  ) : (
+                    <p>
+                      <Link href={"/profile"} className="text-gray-500 text-sm">
+                        You are not a host. To create a host listing, go to your{" "}
+                        <span className="underline">profile page</span> and become an active host.
+                      </Link>
+                    </p>
+                  )}
                 </>
               ) : (
-                <p>
-                  <Link href={"/profile"} className="text-gray-500 text-sm">
-                    You are not a host. To create a host listing, go to your{" "}
-                    <span className="underline">profile page</span> and become an active host.
-                  </Link>
-                </p>
+                <>
+                  <p className="text-gray-500 text-sm">
+                    You are currently not logged in.{" "}
+                    <span className="text-gray-700">
+                      <Link href={"/sign-in"} className="underline">
+                        Log in
+                      </Link>
+                      {"  "}
+                      <Link href={"/sign-up"} className="underline">
+                        Sign up
+                      </Link>
+                    </span>
+                  </p>
+                </>
               )}
-            </>
-          ) : (
-            <>
-              <p className="text-gray-500 text-sm">
-                You are currently not logged in.{" "}
-                <span className="text-gray-700">
-                  <Link href={"/sign-in"} className="underline">
-                    Log in
-                  </Link>
-                  {"  "}
-                  <Link href={"/sign-up"} className="underline">
-                    Sign up
-                  </Link>
-                </span>
-              </p>
-            </>
-          )}
+            </div>
+          </div>
+          <div className="hidden lg:block">{hostListingButton}</div>
         </div>
       </div>
 
