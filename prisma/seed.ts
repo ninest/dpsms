b48f09d7cb03cde817855abca1f24df0d235846c
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-08-20 21:35:24 -0400

    Add listing requests (#34)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -39,29 +39,39 @@ async function main() {
                   "Can self access",
                 ],
               },
-              {
-                address: "28 Flatiron Way",
-                longitude: -71.092838,
-                latitude: 42.338533,
-                timings: "9am - 5pm",
-                sqft: 250,
-                sizeDescription: "Quaint little corner in my closet",
-                qualifiers: ["Has air conditioning", "Is dry", "Has elevator", "Has security", "Is locked"],
-              },
-
-              {
-                address: "945 Jackabrian Avenue",
-                longitude: -71.092938,
-                latitude: 42.338433,
-                timings: "6-10pm on weekdays",
-                sqft: 100,
-                sizeDescription: "Spare room in my brother's car",
-                qualifiers: ["Is dry", "Has elevator", "Has security", "Is locked"],
-              },
             ],
           },
         },
       },
+    },
+    include: {
+      hostUser: true,
+    },
+  });
+
+  const alexanderListing2 = await prisma.hostListing.create({
+    data: {
+      host: { connect: { id: alexanderDB.hostUser?.id } },
+      address: "28 Flatiron Way",
+      longitude: -71.092838,
+      latitude: 42.338533,
+      timings: "9am - 5pm",
+      sqft: 250,
+      sizeDescription: "Quaint little corner in my closet",
+      qualifiers: ["Has air conditioning", "Is dry", "Has elevator", "Has security", "Is locked"],
+    },
+  });
+
+  const alexanderListing3 = await prisma.hostListing.create({
+    data: {
+      host: { connect: { id: alexanderDB.hostUser?.id } },
+      address: "945 Jackabrian Avenue",
+      longitude: -71.092938,
+      latitude: 42.338433,
+      timings: "6-10pm on weekdays",
+      sqft: 100,
+      sizeDescription: "Spare room in my brother's car",
+      qualifiers: ["Is dry", "Has elevator", "Has security", "Is locked"],
     },
   });
 
@@ -95,17 +105,6 @@ async function main() {
                   "Can self access",
                 ],
               },
-
-              {
-                address: "9756 Horroscope Avenue",
-                longitude: -71.092958,
-                latitude: 42.338593,
-                timings: "9am - 9pm",
-                sqft: 175,
-                sizeDescription: "A small spare room in my attic that can fit a few boxes",
-                qualifiers: ["Is dry", "Has elevator", "Has security", "Is locked", "Can self access"],
-              },
-
               {
                 address: "4625 1st Street",
                 longitude: -71.093938,
@@ -115,7 +114,6 @@ async function main() {
                 sizeDescription: "Huge warehouse with lots of space",
                 qualifiers: ["Is dry", "Allows wet items", "Has security", "Is locked", "Can self access"],
               },
-
               {
                 address: "1234 Water Bottle Avenue",
                 longitude: -71.192938,
@@ -125,7 +123,6 @@ async function main() {
                 sizeDescription: "Small closet space that can fit a couple boxes or a person if you're able to fit",
                 qualifiers: ["Has air conditioning", "Is dry", "Has elevator"],
               },
-
               {
                 address: "1234 Water Bottle Avenue",
                 longitude: -71.192938,
@@ -140,6 +137,22 @@ async function main() {
         },
       },
     },
+    include: {
+      hostUser: true,
+    },
+  });
+
+  const parthListing5 = await prisma.hostListing.create({
+    data: {
+      host: { connect: { id: parthDB.hostUser?.id } },
+      address: "9756 Horroscope Avenue",
+      longitude: -71.092958,
+      latitude: 42.338593,
+      timings: "9am - 9pm",
+      sqft: 175,
+      sizeDescription: "A small spare room in my attic that can fit a few boxes",
+      qualifiers: ["Is dry", "Has elevator", "Has security", "Is locked", "Can self access"],
+    },
   });
 
   const arjunDB = await prisma.user.upsert({
@@ -148,6 +161,38 @@ async function main() {
     create: {
       clerkId: arjun.id,
       address: "16 Banana Street",
+      tenantUser: {
+        create: {
+          requests: {
+            create: [
+              {
+                itemsDescription: "A few boxes of clothes",
+                sqft: 10,
+                tenantRequestListing: {
+                  create: [
+                    {
+                      hostListing: { connect: { id: alexanderListing2.id } },
+                      startTime: new Date("2023-08-01T00:00:00.000Z"),
+                      endTime: new Date("2023-09-01T00:00:00.000Z"),
+                    },
+                    {
+                      hostListing: { connect: { id: alexanderListing3.id } },
+                      startTime: new Date("2023-08-01T00:00:00.000Z"),
+                      endTime: new Date("2023-09-01T00:00:00.000Z"),
+                      hostAccepted: true,
+                    },
+                    {
+                      hostListing: { connect: { id: parthListing5.id } },
+                      startTime: new Date("2023-08-01T00:00:00.000Z"),
+                      endTime: new Date("2023-09-01T00:00:00.000Z"),
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
     },
   });
 
