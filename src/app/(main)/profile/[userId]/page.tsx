43eb36c8cb1c47be1930941245349764f7f2ad9d
ninest <parth.kabra@gmail.com>
2023-08-21 13:51:41 -0400

    Tabs

--- src/app/(main)/profile/[userId]/page.tsx
+++ src/app/(main)/profile/[userId]/page.tsx
@@ -1,3 +1,4 @@
+import { roles } from "@/app/role-utils";
 import { ListingCard } from "@/components/ListingCard";
 import { Tenancy } from "@/components/Tenancy";
 import { Empty } from "@/components/empty";
@@ -5,6 +6,7 @@ import { Spacer } from "@/components/spacer";
 import { TenancyRequest } from "@/components/tenancy-request";
 import { Title } from "@/components/typography/title";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { usersService } from "@/services/users-service";
 import { auth, redirectToSignIn } from "@clerk/nextjs";
 import Link from "next/link";
@@ -45,109 +47,118 @@ export default async function UserProfilePage({ children, params }: Props) {
         </Button>
       </div>
 
-      <Spacer className="h-6" />
+      <Spacer className="h-2" />
 
-      {!!hostListings?.length && (
-        <>
-          <div>
-            <Title level={2}>My Host listings</Title>
+      <Tabs defaultValue="account">
+        <TabsList>
+          {roles.map((role) => (
+            <TabsTrigger key={role.slug} value={role.slug}>
+              {role.name}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <Spacer className="h-2" />
+        <TabsContent value="host">
+          {!!hostListings?.length && (
+            <>
+              <div>
+                <Title level={2}>My Host listings</Title>
 
-            <Spacer className="h-2" />
+                <Spacer className="h-2" />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
-              {hostListings.map((listing) => {
-                const numTenantRequests = listing.tenantRequestListing.length;
-                return (
-                  <ListingCard
-                    id={listing.id}
-                    address={listing.address}
-                    timings={listing.timings}
-                    qualifiers={listing.qualifiers}
-                    hostClerkId={listing.host.user.clerkId}
-                    hostTrustedBy={listing.host.user.trustedBy.length}
-                    sqft={listing.sqft}
-                    numTenancyRequests={listing.tenantRequestListing.length}
-                    numTenancies={listing.tenancy.length}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </>
-      )}
-
-      <Spacer className="h-6" />
-
-      {isCurrentUsersPage && (
-        <>
-          <Title level={2}>My requests</Title>
-
-          <Spacer className="h-3" />
-
-          <div className="max-w-[80ch] grid grid-cols-1 gap-3">
-            {(!user.tenantUser || user.tenantUser?.requests.length === 0) && <Empty>No requests</Empty>}
-
-            {user.tenantUser?.requests.map((request) => {
-              return (
-                <Fragment key={request.id}>
-                  {request.tenantRequestListing.map((trl) => {
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+                  {hostListings.map((listing) => {
+                    const numTenantRequests = listing.tenantRequestListing.length;
                     return (
-                      <Link href={`/hosts/${trl.hostListingId}`} className="block">
-                        <TenancyRequest
-                          key={trl.id}
-                          request={{
-                            id: trl.id,
-                            address: trl.hostListing.address,
-                            description: request.itemsDescription,
-                            sqft: request.sqft,
-                            hostAccepted: trl.hostAccepted,
-                            tenantAccepted: trl.tenantAccepted,
-                            startTime: trl.startTime,
-                            endTime: trl.endTime,
-                          }}
-                        />
-                      </Link>
+                      <ListingCard
+                        id={listing.id}
+                        address={listing.address}
+                        timings={listing.timings}
+                        qualifiers={listing.qualifiers}
+                        hostClerkId={listing.host.user.clerkId}
+                        hostTrustedBy={listing.host.user.trustedBy.length}
+                        sqft={listing.sqft}
+                        numTenancyRequests={listing.tenantRequestListing.length}
+                        numTenancies={listing.tenancy.length}
+                      />
                     );
                   })}
-                </Fragment>
-              );
-            })}
-          </div>
-
-          {!!tenancies && (
+                </div>
+              </div>
+            </>
+          )}
+        </TabsContent>
+        <TabsContent value="tenant">
+          {" "}
+          {isCurrentUsersPage && (
             <>
-              <Spacer className="h-6" />
+              <Title level={2}>My requests</Title>
 
-              <Title level={2}>My tenancies</Title>
               <Spacer className="h-3" />
-              {tenancies.length == 0 ? (
+
+              <div className="max-w-[80ch] grid grid-cols-1 gap-3">
+                {(!user.tenantUser || user.tenantUser?.requests.length === 0) && <Empty>No requests</Empty>}
+
+                {user.tenantUser?.requests.map((request) => {
+                  return (
+                    <Fragment key={request.id}>
+                      {request.tenantRequestListing.map((trl) => {
+                        return (
+                          <Link href={`/hosts/${trl.hostListingId}`} className="block">
+                            <TenancyRequest
+                              key={trl.id}
+                              request={{
+                                id: trl.id,
+                                address: trl.hostListing.address,
+                                description: request.itemsDescription,
+                                sqft: request.sqft,
+                                hostAccepted: trl.hostAccepted,
+                                tenantAccepted: trl.tenantAccepted,
+                                startTime: trl.startTime,
+                                endTime: trl.endTime,
+                              }}
+                            />
+                          </Link>
+                        );
+                      })}
+                    </Fragment>
+                  );
+                })}
+              </div>
+
+              {!!tenancies && (
                 <>
-                  <Empty>No tenancies yet</Empty>
-                </>
-              ) : (
-                <>
-                  <div className="space-y-4">
-                    {tenancies.map((t) => (
-                      <Tenancy
-                        id={t.id}
-                        address={t.hostListing.address}
-                        startTime={t.startTime}
-                        endTime={t.endTime}
-                        sqft={t.sqft}
-                        itemsDescription={t.itemsDescription}
-                      />
-                    ))}
-                  </div>
+                  <Spacer className="h-6" />
+
+                  <Title level={2}>My tenancies</Title>
+                  <Spacer className="h-3" />
+                  {tenancies.length == 0 ? (
+                    <>
+                      <Empty>No tenancies yet</Empty>
+                    </>
+                  ) : (
+                    <>
+                      <div className="space-y-4">
+                        {tenancies.map((t) => (
+                          <Tenancy
+                            id={t.id}
+                            address={t.hostListing.address}
+                            startTime={t.startTime}
+                            endTime={t.endTime}
+                            sqft={t.sqft}
+                            itemsDescription={t.itemsDescription}
+                          />
+                        ))}
+                      </div>
+                    </>
+                  )}
                 </>
               )}
             </>
           )}
-        </>
-      )}
-
-      <Spacer className="h-2" />
-
-      {children}
+        </TabsContent>
+        <TabsContent value="mover">Make changes to your account here.</TabsContent>
+      </Tabs>
     </>
   );
 }
